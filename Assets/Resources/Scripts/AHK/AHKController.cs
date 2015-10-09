67037ed95d38e7b53d350cbe156e60f15e197500
Bullshitzu <tolsathaa@gmail.com>
2015-10-09 22:01:57 +0200

    First Commit

---
+++ Assets/Resources/Scripts/AHK/AHKController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AHKController : MonoBehaviour {
+	
+	public GameObject engineLeft;
+	public GameObject engineRight;
+	
+	public GameObject engineLeftFan;
+	public GameObject engineRightFan;
+	
+	// other references here...
+	
+	void Update () {
+		
+		
+		
+	}
+	
+}
